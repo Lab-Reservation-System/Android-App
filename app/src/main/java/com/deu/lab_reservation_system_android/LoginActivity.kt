365f42a0742dc48f14801aea100c3b831b23dfa0
rlawns0327 <rlawns0327@naver.com>
2022-11-08 20:53:41 +0900

    로그인 화면

---
+++ app/src/main/java/com/deu/lab_reservation_system_android/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.deu.lab_reservation_system_android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+
+    // 엑티비티가 생성되었을 때
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // 레이아웃을 설정
+        setContentView(R.layout.login)
+
+    }
+}
