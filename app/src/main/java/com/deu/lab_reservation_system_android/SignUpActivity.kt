9e6f907aaba2d2a56ecfbaa4fb4574000276525e
Minsu <jminsu084@gmail.com>
2022-11-09 20:42:18 +0900

    fixed untracked files

---
+++ app/src/main/java/com/deu/lab_reservation_system_android/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.deu.lab_reservation_system_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
