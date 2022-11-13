d2309fe1a6e3c6c50d8b5fefda2499c563144abb
Minsu <jminsu084@gmail.com>
2022-11-14 02:49:13 +0900

    사용자 관리 화면 완료 (Table Row 동적 추가)

---
+++ app/src/main/java/com/deu/lab_reservation_system_android/activity/User/StudentUser.kt
@@ -0,0 +1,8 @@
+package com.deu.lab_reservation_system_android.activity.User
+
+data class StudentUser(
+    var name: String,
+    var stdNum: String,
+    var pwd: String,
+    var check: Boolean
+)
