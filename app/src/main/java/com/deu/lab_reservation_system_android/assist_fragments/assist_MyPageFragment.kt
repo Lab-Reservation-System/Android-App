2f0ebdd84b4c15feb1e8fb1e58f9ef559787dd74
rlawns0327 <rlawns0327@naver.com>
2022-11-16 06:25:26 +0900

    로그아웃 처리

--- app/src/main/java/com/deu/lab_reservation_system_android/assist_fragments/assist_MyPageFragment.kt
+++ app/src/main/java/com/deu/lab_reservation_system_android/assist_fragments/assist_MyPageFragment.kt
@@ -1,11 +1,13 @@
 package com.deu.lab_reservation_system_android.assist_fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.deu.lab_reservation_system_android.activity.User.LoginActivity
 import com.deu.lab_reservation_system_android.activity.nav.Assistant_Nav_Activity
 import com.deu.lab_reservation_system_android.activity.nav.Student_Nav_Activity
 import com.deu.lab_reservation_system_android.databinding.FragmentAssistMypageBinding
@@ -32,6 +34,11 @@ class assist_MyPageFragment : Fragment() {
         binding.userEmail.setText(user.email)
         binding.userName.setText(user.name)
         binding.userPhone.setText(user.phoneNumber)
+
+        binding.logoutBtn.setOnClickListener(){
+            var intent = Intent(activity, LoginActivity::class.java)
+            startActivity(intent)
+        }
 
         return mBinding?.root
 
