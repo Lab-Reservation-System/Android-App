956e1ff81a172259e73d8db784e9680d3f1f91b8
Minsu <jminsu084@gmail.com>
2022-11-20 03:56:27 +0900

    전체 수업 조회 api

--- app/src/main/java/com/deu/lab_reservation_system_android/ClassesAPI_Controller.kt
+++ app/src/main/java/com/deu/lab_reservation_system_android/ClassesAPI_Controller.kt
@@ -1,12 +1,18 @@
 package com.deu.lab_reservation_system_android
 
+import com.deu.lab_reservation_system_android.model.Classes
 import com.deu.lab_reservation_system_android.model.Dto.ClassesDto
+import com.deu.lab_reservation_system_android.model.Dto.Reservation
 import com.deu.lab_reservation_system_android.model.Dto.SignupDto
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface ClassesAPI_Controller {
     @POST("/api/class/create")
     fun getCreateClassResponse(@Body classes: MutableList<ClassesDto>): Call<String>
+
+    @GET("/api/class/index")
+    fun getAllSeminarClassResponse() : Call<List<Classes>>
 }
---
+++ app/src/main/java/com/deu/lab_reservation_system_android/model/Classes.kt
@@ -0,0 +1,13 @@
+package com.deu.lab_reservation_system_android.model
+
+data class Classes(
+    val className: Any,
+    val classNum: Int,
+    val date: String,
+    val labNumber: String,
+    val regularClassNum: Int,
+    val time: String,
+    val type: String,
+    val userId: String,
+    val userName: String
+)
